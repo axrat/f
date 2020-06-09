7be3b4b60f130cdf1967c973e54ddb8711a76567
onoie <onoie3@gmail.com>
2020-06-09 19:43:02 +0900

    fast commit

--- docs/f.sh
+++ docs/f.sh
@@ -54,7 +54,7 @@ wgetgithub(){
 alias ll='ls -alFh'
 alias la='ls -A'
 alias l='ls -cf'
-alias watch='watch -d -n 3'
+alias watch='watch -d=cumulative -n 5'
 alias ps="ps --sort=start_time"
 alias v="vim"
 alias e="emacs -nw"
@@ -1030,7 +1030,7 @@ herokupointdns(){
 LOADED+=('f')
 f(){
 	hr
-	echo VERSION:2020-06-06 16:24:27.304268200
+	echo VERSION:2020-06-09 19:42:43.460794645
 	hr
 }
 #!/bin/bash
