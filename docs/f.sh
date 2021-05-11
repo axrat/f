3da78d124d5aaa3b93cdd1298cadc74c5ae09bb8
onoie <onoie3@gmail.com>
2021-05-11 19:19:45 +0900

    fast commit

--- docs/f.sh
+++ docs/f.sh
@@ -33,6 +33,8 @@ gitinitialize(){
   if [ $# -ne 6 ]; then
     echo "Require [host],[user],[repo],[branch],[name],[email]"
   else
+    BASE=$(cd $(dirname $0); pwd)
+    cd $BASE
     rm -rf .git
     git init
     git remote add origin git@$1:$2/$3.git
@@ -1031,7 +1033,7 @@ directory_size(){
 LOADED+=('f')
 f(){
   hr
-  echo VERSION:2021-05-11 19:17:21.706265100
+  echo VERSION:2021-05-11 19:19:43.124315700
   hr
 }
 #!/bin/bash
--- require/gitinitialize.sh
+++ require/gitinitialize.sh
@@ -3,6 +3,8 @@ gitinitialize(){
   if [ $# -ne 6 ]; then
     echo "Require [host],[user],[repo],[branch],[name],[email]"
   else
+    BASE=$(cd $(dirname $0); pwd)
+    cd $BASE
     rm -rf .git
     git init
     git remote add origin git@$1:$2/$3.git
